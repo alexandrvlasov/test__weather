16161b5fa098f3aefe6c7aeafa4aa98d7b4807b8
alexandrvlasov <alexandrvlasovv05@gmail.com>
2024-11-29 23:20:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -66,6 +66,15 @@ function displayWeatherData(data) {
     header.insertAdjacentHTML('afterend', html);
 
     initMap(data.location.lat, data.location.lon);
+    
+    const deleteButton = header.nextElementSibling.querySelector('.del__button');
+    
+    deleteButton.onclick = function() {
+        const card = deleteButton.closest('.card');
+        if (card) {
+            card.remove();
+        }
+    };
 }
 
 function createWeatherCard(data, info, imgPath) {
@@ -80,6 +89,7 @@ function createWeatherCard(data, info, imgPath) {
                     <img class="card__weather" src="${imgPath}" width="318" height="318" alt="Погода">
                 </div>
                 <div id="map"></div>
+                <button class="del__button">Удалить карточку</button>
             </div>`;
 }
 
